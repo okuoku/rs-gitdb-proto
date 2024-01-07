9d8fbaebcd4e9c6fd9b7cde8fabc374f2b2f2858
okuoku <mjt@cltn.org>
2024-01-07 20:25:30 +0900

    Add reader

--- _check.mjs
+++ _check.mjs
@@ -8,3 +8,17 @@ await writer.set("a/bbb.json", JSON.stringify(1234));
 await writer.set("a/ccc.json", JSON.stringify({mypid: process.pid}));
 await writer.commit({msg: "Testing"});
 await writer.dispose();
+
+const reader = gitdb.make_reader("./check");
+
+async function dbg(a, b){
+    const obj = await reader.get(a, b);
+    console.log(a,b,obj.toString("utf8"));
+    return true;
+}
+
+await dbg("refs/heads/master", "a/ccc.json");
+await dbg("refs/heads/master^1", "a/ccc.json");
+await dbg("refs/heads/master", "a/xccc.json");
+await dbg("refs/heads/master0", "a/xccc.json");
+await reader.dispose();
--- gitdb.mjs
+++ gitdb.mjs
@@ -138,7 +138,6 @@ function make_writer(gitdir, index_name){
             // rm $index_file
             const p = new Promise((res, rej) => {
                 fs.rm(index_file, {force: false}, (err) => {
-                    console.log("Removed", err, index_file);
                     // Ignore error
                     res();
                 });
@@ -149,6 +148,105 @@ function make_writer(gitdir, index_name){
     };
 }
 
+function make_reader(gitdir){
+    let errstate = false;
+    let recv = false;
+    const child = child_process.spawn("git", ["cat-file", "--batch"], 
+                                      {cwd: gitdir, stdio: ["pipe", "pipe", "inherit"]});
+
+
+    child.on("error", () => {
+        errstate = true;
+        child = null;
+    });
+
+    let in_header = true;
+    let headerbuf = [];
+    let bodybuf = [];
+    let bodysize = 0;
+    let queuedsize = 0;
+    child.stdout.on("data", (dat) => {
+        function consumebody(buf){
+            if((bodysize + 1) == queuedsize + buf.length){
+                if(buf.length != 1){
+                    bodybuf.push(dat.subarray(0, buf.length - 1));
+                }
+                const out = Buffer.concat(bodybuf);
+                bodybuf = [];
+                queuedsize = 0;
+                in_header = true;
+                recv(out);
+            }else{
+                bodybuf.push(buf);
+                queuedsize += buf.length;
+                if(queuedsize > bodysize){
+                    throw "something wrong";
+                }
+            }
+        }
+        function parseheader(){
+            const re = /([0-9a-f]+) blob ([0-9]+)/;
+            const header = Buffer.concat(headerbuf).toString("utf8");
+            headerbuf = [];
+            const m = header.match(re);
+            if(m){
+                bodysize = parseInt(m[2]);
+                in_header = false;
+            }else{
+                bodysize = 0;
+            }
+        }
+
+        if(in_header){
+            // Read to LF
+            const lfidx = dat.indexOf(0x0a /* LF */);
+            if(lfidx == -1){
+                // Partial header
+                headerbuf.push(dat);
+            }else{
+                if(lfidx != 0){
+                    headerbuf.push(dat.subarray(0, lfidx));
+                }
+                parseheader(); // should now have bodysize
+                if(bodysize != false){
+                    if(lfidx+1 != dat.length){
+                        consumebody(dat.subarray(lfidx + 1, dat.length));
+                    }
+                }else{
+                    // error. Return false as data
+                    recv(false);
+                }
+            }
+        }else{
+            consumebody(dat);
+        }
+    });
+
+
+    return {
+        get: async function(ref, path){
+            const writedata = ref + ":" + path + "\n";
+            return new Promise((res, rej) => {
+                if(recv){
+                    rej("overlapped");
+                }else{
+                    recv = function(blob){
+                        recv = false;
+                        res(blob);
+                    };
+                    child.stdin.write(writedata, "utf8");
+                }
+            });
+        },
+        dispose: async function(){
+            return new Promise((res, rej) => {
+                child.stdin.end(x => res());
+            });
+        }
+    }
+}
+
 export default {
-    make_writer: make_writer
+    make_writer: make_writer,
+    make_reader: make_reader
 };
